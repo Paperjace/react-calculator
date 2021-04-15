bca64b7bb9636653f811b830f4146c1eeba4197f
Jason Hurley <paperjace@gmail.com>
2021-04-15 16:18:31 -0400

    Moved button creation out of constructor and wrote them out literally in render() function in Calculator

--- src/index.js
+++ src/index.js
@@ -3,15 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Button(props) {
-  if(props.buttonDisplay === 'clear'){
-    return <div className="buttonClear" onClick={props.onClick}>{props.buttonDisplay}</div>
-  }
-
-  if(['÷', 'x','-', '+', '='].indexOf(props.buttonDisplay) +1) {
-    return <div className="buttonSecondary" onClick={props.onClick}>{props.buttonDisplay}</div>
-  }
-  
-  return <div className="buttonPrimary" onClick={props.onClick}>{props.buttonDisplay}</div>
+  return <div className={props.className} onClick={props.onClick}>{props.buttonDisplay}</div>
 }
 
 function LCDDisplay(props) {
@@ -22,70 +14,39 @@ class Calculator extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      memory: [0]
+      memory: ['0']
     }
-    
-    this.buttonConfig = [
-      'clear',
-      '1', '2', '3', '÷', 
-      '4', '5', '6', 'x', 
-      '7', '8', '9', '-', 
-      '0', '.', "=", "+"]
-
-    this.buttons = this.buttonConfig.map((config, index) => {
-
-      if(config === 'clear') {
-        return <Button key={index} buttonDisplay={config} onClick={this.clear}/>
-      }
-      if(config === '÷') {
-        return <Button key={index} buttonDisplay={config} onClick={this.divide}/>
-      }
-      if(config === 'x') {
-        return <Button key={index} buttonDisplay={config} onClick={this.multiply}/>
-      }
-      if(config === '-') {
-        return <Button key={index} buttonDisplay={config} onClick={this.subtract}/>
-      }
-      if(config === '+') {
-        return <Button key={index} buttonDisplay={config} onClick={this.add}/>
-      }
-      if(config === '=') {
-        return <Button key={index} buttonDisplay={config} onClick={this.sum}/>
-      }
-
-      return <Button key={index} buttonDisplay={config} onClick={() => this.numpad(config)}/>
-    });
   }
 
-  clear(){
+  clear = () => {
     this.setState({
-      memory: []
+      memory: [0]
     })
   }
 
-  divide(){
+  divide = () => {
     alert('you clicked DIVIDE')
   }
 
-  multiply(){
+  multiply = () => {
     alert('you clicked MULTIPLY')
   }
 
-  subtract(){
+  subtract = () => {
     alert('you clicked SUBTRACT')
   }
 
-  add(){
+  add = () => {
     alert('you clicked ADD')
   }
 
-  sum(){
-    alert('you clicked SUM')
+  equals = () => {
+    alert('you clicked EQUALS')
   }
 
-  numpad(number){
+  numpad = (number) => {
     this.setState({
-      memory: this.state.memory.concat(number)
+      memory:this.state.memory.concat(number)
     })
   }
 
@@ -93,7 +54,26 @@ class Calculator extends React.Component {
     return (
       <div className="container">
         <LCDDisplay memory={this.state.memory}/>
-        {this.buttons}
+        <Button className="buttonClear" buttonDisplay="clear" onClick={this.clear}/>
+        <Button className="buttonPrimary" buttonDisplay="1" onClick={() => this.numpad(1)}/>
+        <Button className="buttonPrimary" buttonDisplay="2" onClick={() => this.numpad(2)}/>
+        <Button className="buttonPrimary" buttonDisplay="3" onClick={() => this.numpad(3)}/>
+        <Button className="buttonSecondary" buttonDisplay="/" onClick={this.divide}/>
+
+        <Button className="buttonPrimary" buttonDisplay="4" onClick={() => this.numpad(4)}/>
+        <Button className="buttonPrimary" buttonDisplay="5" onClick={() => this.numpad(5)}/>
+        <Button className="buttonPrimary" buttonDisplay="6" onClick={() => this.numpad(6)}/>
+        <Button className="buttonSecondary" buttonDisplay="*" onClick={this.multiply}/>
+
+        <Button className="buttonPrimary" buttonDisplay="7" onClick={() => this.numpad(7)}/>
+        <Button className="buttonPrimary" buttonDisplay="8" onClick={() => this.numpad(8)}/>
+        <Button className="buttonPrimary" buttonDisplay="9" onClick={() => this.numpad(9)}/>
+        <Button className="buttonSecondary" buttonDisplay="-" onClick={this.subtract}/>
+
+        <Button className="buttonPrimary" buttonDisplay="." onClick={() => this.numpad(".")}/>
+        <Button className="buttonPrimary" buttonDisplay="0" onClick={() => this.numpad(0)}/>
+        <Button className="buttonPrimary" buttonDisplay="=" onClick={this.equals}/>
+        <Button className="buttonSecondary" buttonDisplay="+" onClick={this.add}/>        
       </div>
     )
   }  
